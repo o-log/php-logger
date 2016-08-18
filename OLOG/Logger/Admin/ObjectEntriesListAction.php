d20148665094953209bac57e7179c02dea91f53c
ologinov <olognv@gmail.com>
2016-08-18 16:44:09 +0300

    1

---
+++ OLOG/Logger/Admin/ObjectEntriesListAction.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace OLOG\Logger\Admin;
+
+use OLOG\Auth\Operator;
+use OLOG\BT\InterfaceBreadcrumbs;
+use OLOG\BT\InterfacePageTitle;
+use OLOG\BT\Layout;
+use OLOG\CRUD\CRUDTableFilter;
+use OLOG\Exits;
+
+class ObjectEntriesListAction implements
+    InterfaceBreadcrumbs,
+    InterfacePageTitle//,
+    //InterfaceUserName
+{
+    //use CurrentUserNameTrait;
+
+    static public function getUrl($object_fullid = '([\w\.%]+)'){
+        return '/admin/logger/objectentries/' . $object_fullid;
+    }
+
+    public function currentPageTitle()
+    {
+        return self::pageTitle();
+    }
+
+    static public function pageTitle(){
+        return 'Logger entries';
+    }
+
+    public function currentBreadcrumbsArr(){
+        return self::breadcrumbsArr();
+    }
+
+    static public function breadcrumbsArr()
+    {
+        //return array_merge(AuthAdminAction::breadcrumbsArr(), [BT::a(self::getUrl(), self::pageTitle())]);
+        return [];
+    }
+
+
+    public function action($object_fullid){
+        Exits::exit403If(
+            !Operator::currentOperatorHasAnyOfPermissions([\OLOG\Logger\Permissions::PERMISSION_PHPLOGGER_ACCESS])
+        );
+
+        $html = \OLOG\CRUD\CRUDTable::html(
+            \OLOG\Logger\Entry::class,
+            '',
+            [
+                new \OLOG\CRUD\CRUDTableColumn(
+                    'user_fullid',
+                    new \OLOG\CRUD\CRUDTableWidgetTextWithLink('{this->user_fullid}', EntryEditAction::getUrl('{this->id}'))
+                ),
+                new \OLOG\CRUD\CRUDTableColumn(
+                    'object_fullid',
+                    new \OLOG\CRUD\CRUDTableWidgetText('{this->object_fullid}')
+                ),
+                new \OLOG\CRUD\CRUDTableColumn(
+                    'created at',
+                    new \OLOG\CRUD\CRUDTableWidgetText('{this->created_at_ts}')
+                )
+            ],
+            [
+                new CRUDTableFilter('object_fullid', CRUDTableFilter::FILTER_EQUAL, $object_fullid)
+            ],
+            'created_at_ts desc'
+        );
+
+        Layout::render($html, $this);
+    }
+}
