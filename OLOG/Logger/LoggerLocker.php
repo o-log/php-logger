fbc90e092c7d8598f1b4b1014a46086ff60c8131
romanpetrakov <namor.prs@gmail.com>
2016-11-09 20:23:32 +0300

    IMB-442.1

--- OLOG/Logger/LoggerLocker.php
+++ OLOG/Logger/LoggerLocker.php
@@ -8,11 +8,16 @@ class LoggerLocker
 
     public static function lock($object_name)
     {
-        if ( (array_key_exists(self::$locked_objects_arr, $object_name))){
+        if (self::isLocked($object_name)){
             return false;
         }
         self::$locked_objects_arr[$object_name] = true;
         return true;
+    }
+
+    public static function isLocked($object_name)
+    {
+        return array_key_exists($object_name, self::$locked_objects_arr);
     }
 
     public static function unlock($object_name)
